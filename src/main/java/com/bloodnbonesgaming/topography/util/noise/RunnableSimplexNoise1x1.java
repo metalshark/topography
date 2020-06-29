8e8b3232330403f9d67fbc81ef0df566a87df732
Blargerist Platypus <yaywarcraft@gmail.com>
2020-06-28 18:58:50 -0600

    1.14.1 Change lossless simplex noise to 4 layer Update Basic_Sky_Islands preset Update Volcano_World preset

--- src/main/java/com/bloodnbonesgaming/topography/util/noise/RunnableSimplexNoise1x1.java
+++ src/main/java/com/bloodnbonesgaming/topography/util/noise/RunnableSimplexNoise1x1.java
@@ -19,17 +19,19 @@ public class RunnableSimplexNoise1x1 implements Runnable{
 	final double persistence;
 	final double coordinateScale;
 	final double[] layerArray;
+	final int layerThickness;
 
-	public RunnableSimplexNoise1x1( final OpenSimplexNoiseGeneratorOctaves noise, final int startX, final int startY, final int startZ, final double coordinateScale, final int octaves, final double persistence)
+	public RunnableSimplexNoise1x1( final OpenSimplexNoiseGeneratorOctaves noise, final int layerThickness, final int startX, final int startY, final int startZ, final double coordinateScale, final int octaves, final double persistence)
 	{
 		this.noise = noise;
+		this.layerThickness = layerThickness;
         this.startX = startX;
         this.startY = startY;
         this.startZ = startZ;
         this.coordinateScale = coordinateScale;
         this.octaves = octaves;
         this.persistence = persistence;
-        this.layerArray = new double[16 * 16 * 8];
+        this.layerArray = new double[16 * 16 * layerThickness];
 	}
 	
 	@Override
@@ -37,23 +39,23 @@ public class RunnableSimplexNoise1x1 implements Runnable{
 		
 		for (int x = 0; x < 16; x++) {
 			for (int z = 0; z < 16; z++) {
-				for (int y = 0; y < 8; y++) {
+				for (int y = 0; y < this.layerThickness; y++) {
 					//int index = (xI * arraySizeX + zI) * arraySizeY + yI;
-					this.layerArray[(x * 16 + z) * 8 + y] = this.noise.eval((startX + x) / coordinateScale, (startY + y) / coordinateScale, (startZ + z) / coordinateScale, octaves, persistence);
+					this.layerArray[(x * 16 + z) * this.layerThickness + y] = this.noise.eval((startX + x) / coordinateScale, (startY + y) / coordinateScale, (startZ + z) / coordinateScale, octaves, persistence);
 				}
 			}
 		}
-		RunnableSimplexNoise1x1.addToArray(this.layerArray, this.startY);
+		RunnableSimplexNoise1x1.addToArray(this.layerArray, this.layerThickness, this.startY);
 	}
 	
 	private static double[] array = null;
 	
-	public static synchronized void addToArray(final double[] layerArray, final int startY)
+	public static synchronized void addToArray(final double[] layerArray, final int layerThickness, final int startY)
 	{
 		for (int x = 0; x < 16; x++) {
 			for (int z = 0; z < 16; z++) {
-				for (int y = 0; y < 8; y++) {
-					RunnableSimplexNoise1x1.array[(x * 16 + z) * 256 + startY + y] = layerArray[(x * 16 + z) * 8 + y];
+				for (int y = 0; y < layerThickness; y++) {
+					RunnableSimplexNoise1x1.array[(x * 16 + z) * 256 + startY + y] = layerArray[(x * 16 + z) * layerThickness + y];
 				}
 			}
 		}
@@ -61,12 +63,13 @@ public class RunnableSimplexNoise1x1 implements Runnable{
 	
 	public static void getNoise(final double[] array, final OpenSimplexNoiseGeneratorOctaves generator, final int firstY, final int lastY, final int startX, final int startZ, final double coordinateScale, final int octaves, final double persistence)
 	{
+		final int layerThickness = 4;
 		RunnableSimplexNoise1x1.array = array;
 		final List<Callable<Object>> callables = new ArrayList<Callable<Object>>();
 		
 		//Generates noise 
-		for (int y = firstY / 8 * 8; y < lastY; y += 8) {
-			callables.add(Executors.callable(new RunnableSimplexNoise1x1(generator, startX, y, startZ, coordinateScale, octaves, persistence)));
+		for (int y = firstY / layerThickness * layerThickness; y < lastY; y += layerThickness) {
+			callables.add(Executors.callable(new RunnableSimplexNoise1x1(generator, layerThickness, startX, y, startZ, coordinateScale, octaves, persistence)));
 		}
 		
 		try {

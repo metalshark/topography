af6edb85c039f27c360166bab19a7c41883f0571
Blargerist Platypus <yaywarcraft@gmail.com>
2020-11-11 00:12:55 -0600

    1.10.0 Modify background replacement to use Core injections so it covers more guis and doesn't replace gradients.

---
+++ src/main/java/com/bloodnbonesgaming/topography/client/core/ClientHooks.java
@@ -0,0 +1,125 @@
+package com.bloodnbonesgaming.topography.client.core;
+
+import com.bloodnbonesgaming.topography.client.gui.element.EnumGuiLocation;
+import com.bloodnbonesgaming.topography.client.gui.element.GuiElementTexture;
+import com.bloodnbonesgaming.topography.client.gui.element.GuiElementTextureStretch;
+import com.bloodnbonesgaming.topography.common.config.ConfigurationManager;
+import com.bloodnbonesgaming.topography.common.config.DimensionDef;
+import com.bloodnbonesgaming.topography.common.config.GlobalConfig;
+import com.bloodnbonesgaming.topography.common.config.Preset;
+import com.bloodnbonesgaming.topography.common.util.IOHelper;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.widget.list.AbstractList;
+import net.minecraft.client.renderer.texture.DynamicTexture;
+import net.minecraft.client.renderer.texture.MissingTextureSprite;
+import net.minecraft.client.renderer.texture.NativeImage;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.World;
+
+public class ClientHooks {
+	
+	private static ResourceLocation backgroundLoc = null;
+	private static GuiElementTexture texture = null;
+	private static String currentImage = null;
+
+	public static void onAbstractListBackgroundDrawn(AbstractList<?> list) {
+		list.field_244603_t = false;
+		list.renderHeader = false;
+		list.field_244604_u = false;
+		
+		Minecraft minecraft = list.minecraft;
+		
+		if (backgroundLoc == null) {
+			backgroundLoc = minecraft.getTextureManager().getDynamicTextureLocation("background_img", MissingTextureSprite.getDynamicTexture());
+		}
+		final GlobalConfig config = ConfigurationManager.getGlobalConfig();
+		
+		if (config != null) {
+			String imageName = null;
+			
+			if (config.getGuiBackground() != null) {
+				imageName = config.getGuiBackground();
+			}
+			final Preset preset = config.getPreset();
+			
+			if (preset != null) {
+				if (preset.getGuiBackground() != null) {
+					imageName = preset.getGuiBackground();
+				}
+				World world = minecraft.world;
+				
+				if (world != null) {
+					DimensionDef def = preset.defs.get(world.getDimensionKey().getLocation());
+					
+					if (def != null) {
+						if (def.getGuiBackground() != null) {
+							imageName = def.getGuiBackground();
+						}
+					}
+				}
+			}
+			
+			if (imageName != null && !imageName.isEmpty() && !imageName.equals(currentImage)) {
+				final NativeImage image = IOHelper.loadNativeImage(imageName);
+
+				if (image != null) {
+					minecraft.getTextureManager().loadTexture(backgroundLoc, new DynamicTexture(image));
+					ClientHooks.texture = new GuiElementTextureStretch(EnumGuiLocation.TOP_LEFT, backgroundLoc, image.getWidth(), image.getHeight());
+					//ClientHooks.texture = new GuiElementTextureStretch(EnumGuiLocation.TOP_LEFT, new ResourceLocation("textures/gui/options_background.png"), image.getWidth(), image.getHeight());
+					ClientHooks.texture.setRelRender(1, 1);
+				}
+			}
+		}
+		if (texture != null) {
+			texture.render(minecraft, list.width, list.height);
+		}
+	}
+	
+	public static void onBackgroundDrawn(final Screen gui) {
+		if (backgroundLoc == null) {
+			backgroundLoc = gui.getMinecraft().getTextureManager().getDynamicTextureLocation("background_img", MissingTextureSprite.getDynamicTexture());
+		}
+		final GlobalConfig config = ConfigurationManager.getGlobalConfig();
+		
+		if (config != null) {
+			String imageName = null;
+			
+			if (config.getGuiBackground() != null) {
+				imageName = config.getGuiBackground();
+			}
+			final Preset preset = config.getPreset();
+			
+			if (preset != null) {
+				if (preset.getGuiBackground() != null) {
+					imageName = preset.getGuiBackground();
+				}
+				World world = gui.getMinecraft().world;
+				
+				if (world != null) {
+					DimensionDef def = preset.defs.get(world.getDimensionKey().getLocation());
+					
+					if (def != null) {
+						if (def.getGuiBackground() != null) {
+							imageName = def.getGuiBackground();
+						}
+					}
+				}
+			}
+			
+			if (imageName != null && !imageName.isEmpty() && !imageName.equals(currentImage)) {
+				final NativeImage image = IOHelper.loadNativeImage(imageName);
+
+				if (image != null) {
+					gui.getMinecraft().getTextureManager().loadTexture(backgroundLoc, new DynamicTexture(image));
+					texture = new GuiElementTextureStretch(EnumGuiLocation.TOP_LEFT, backgroundLoc, image.getWidth(), image.getHeight());
+					texture.setRelRender(1, 1);
+				}
+			}
+		}
+		if (texture != null) {
+			texture.render(gui.getMinecraft(), gui.width, gui.height);
+		}
+	}
+}
--- src/main/resources/topography_transformer.js
+++ src/main/resources/topography_transformer.js
@@ -205,6 +205,95 @@ function initializeCoreMod() {
         		
                 return method;
             }
+        },
+        'abstractlistrender': {
+            'target': {
+                'type': 'METHOD',
+                'class': 'net.minecraft.client.gui.widget.list.AbstractList',
+                'methodName': 'func_230430_a_',
+                'methodDesc': '(Lcom/mojang/blaze3d/matrix/MatrixStack;IIF)V'
+            },
+            'transformer': function(method) {
+                print("Topography injecting into: " + method.name);
+            	
+            	var ASMAPI = Java.type('net.minecraftforge.coremod.api.ASMAPI');
+            	var Opcodes = Java.type('org.objectweb.asm.Opcodes');
+            	var LabelNode = Java.type('org.objectweb.asm.tree.LabelNode');
+            	var InsnList = Java.type('org.objectweb.asm.tree.InsnList');
+            	var VarInsnNode = Java.type('org.objectweb.asm.tree.VarInsnNode');
+            	var JumpInsnNode = Java.type('org.objectweb.asm.tree.JumpInsnNode');
+            	var InsnNode = Java.type('org.objectweb.asm.tree.InsnNode');
+            	var FieldInsnNode = Java.type('org.objectweb.asm.tree.FieldInsnNode');
+            	
+            	/*var target = ASMAPI.findFirstMethodCall(method, ASMAPI.MethodType.VIRTUAL, "net/minecraft/client/gui/widget/list/AbstractList", "getRowLeft", "()I");
+            	target = target.getPrevious();
+        		
+        		if (target == null)
+        		{
+        			throw "Something went wrong in Topography";
+        		}
+            	
+            	var toInject = new InsnList();
+            	
+            	toInject.add(new VarInsnNode(Opcodes.ALOAD, 0));
+            	toInject.add(new FieldInsnNode(Opcodes.GETFIELD, "net/minecraft/client/gui/widget/list/AbstractList", "field_244603_t", "Z"))
+            	var label = new LabelNode();
+            	toInject.add(new JumpInsnNode(Opcodes.IFEQ, label));
+
+            	toInject.add(new VarInsnNode(Opcodes.ALOAD, 0));
+            	toInject.add(ASMAPI.buildMethodCall("com/bloodnbonesgaming/topography/client/core/ClientHooks", "onAbstractListBackgroundDrawn", "(Lnet/minecraft/client/gui/widget/list/AbstractList;)V", ASMAPI.MethodType.STATIC));
+            	toInject.add(label);*/
+            	
+            	var target = ASMAPI.findFirstMethodCall(method, ASMAPI.MethodType.VIRTUAL, "net/minecraft/client/gui/widget/list/AbstractList", "renderBackground", "(Lcom/mojang/blaze3d/matrix/MatrixStack;)V");
+        		
+        		if (target == null)
+        		{
+        			throw "Something went wrong in Topography";
+        		}
+            	
+            	var toInject = new InsnList();
+            	toInject.add(new VarInsnNode(Opcodes.ALOAD, 0));
+            	toInject.add(ASMAPI.buildMethodCall("com/bloodnbonesgaming/topography/client/core/ClientHooks", "onAbstractListBackgroundDrawn", "(Lnet/minecraft/client/gui/widget/list/AbstractList;)V", ASMAPI.MethodType.STATIC));
+            	
+        		method.instructions.insert(target, toInject);
+        		
+                return method;
+            }
+        },
+        'screenrenderdirtbackground': {
+            'target': {
+                'type': 'METHOD',
+                'class': 'net.minecraft.client.gui.screen.Screen',
+                'methodName': 'func_231165_f_',
+                'methodDesc': '(I)V'
+            },
+            'transformer': function(method) {
+                print("Topography injecting into: " + method.name);
+            	
+            	var ASMAPI = Java.type('net.minecraftforge.coremod.api.ASMAPI');
+            	var Opcodes = Java.type('org.objectweb.asm.Opcodes');
+            	var LabelNode = Java.type('org.objectweb.asm.tree.LabelNode');
+            	var InsnList = Java.type('org.objectweb.asm.tree.InsnList');
+            	var VarInsnNode = Java.type('org.objectweb.asm.tree.VarInsnNode');
+            	var JumpInsnNode = Java.type('org.objectweb.asm.tree.JumpInsnNode');
+            	var InsnNode = Java.type('org.objectweb.asm.tree.InsnNode');
+            	var FieldInsnNode = Java.type('org.objectweb.asm.tree.FieldInsnNode');
+            	
+            	var target = ASMAPI.findFirstMethodCall(method, ASMAPI.MethodType.VIRTUAL, "net/minecraft/client/renderer/Tessellator", "draw", "()V");
+        		
+        		if (target == null)
+        		{
+        			throw "Something went wrong in Topography";
+        		}
+            	
+            	var toInject = new InsnList();
+            	toInject.add(new VarInsnNode(Opcodes.ALOAD, 0));
+            	toInject.add(ASMAPI.buildMethodCall("com/bloodnbonesgaming/topography/client/core/ClientHooks", "onBackgroundDrawn", "(Lnet/minecraft/client/gui/screen/Screen;)V", ASMAPI.MethodType.STATIC));
+            	
+        		method.instructions.insert(target, toInject);
+        		
+                return method;
+            }
         }
     }
 }
